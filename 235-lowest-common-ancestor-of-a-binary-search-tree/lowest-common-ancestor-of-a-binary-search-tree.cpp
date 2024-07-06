c3cdcf3c16aba58250be26a6c5b5dc2854c8ae1a
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-07-07 02:12:34 +0530

    Time: 20 ms (81.25%) | Memory: 21.7 MB (64.13%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,7 +7,12 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
+#pragma GCC optimize("Ofast,unroll-loops") 
+int speed_up = []{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    return 0;
+}();
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
