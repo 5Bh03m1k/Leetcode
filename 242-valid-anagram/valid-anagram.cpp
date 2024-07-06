3b9396b20db35108198493d2ec33bb1468b082ed
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-07-06 17:34:05 +0530

    Time: 8 ms (64.14%) | Memory: 8.5 MB (83.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
-    bool chk(string s, string t)
-    {
-        int a[26]={0},n=s.size();
+    bool isAnagram(string s, string t) {
+        
+          if( s.size()==t.size()) 
+        {
+            int a[26]={0},n=s.size();
         for(int i=0;i<n;i++)
             a[s[i]-'a']++,a[t[i]-'a']--;
         for(int i=0; i<26;i++)
@@ -11,11 +13,7 @@ public:
                 return false;
         }
         return true;
-    }
-    bool isAnagram(string s, string t) {
-        
-          if( s.size()==t.size()) 
-          return chk(s,t);
+        }
           return false;
         
     }
