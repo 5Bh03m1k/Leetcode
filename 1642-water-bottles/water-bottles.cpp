c841355286fc09236e6e737904053074e92e1913
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-07-07 14:13:26 +0530

    Time: 2 ms (53.84%) | Memory: 7.2 MB (30.43%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int numWaterBottles(int n, int t,int k=0) {
-        if((n+k)<t)
-            return n;
-        return  n+numWaterBottles((n+k)/t,t,(n+k)%t);
+        return  (n+k)>=t?(n+numWaterBottles((n+k)/t,t,(n+k)%t)):n;
     }
 };
