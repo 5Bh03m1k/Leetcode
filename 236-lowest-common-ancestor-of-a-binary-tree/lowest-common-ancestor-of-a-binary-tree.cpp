10a05f568dd75b92719d578c4504463694ab8a1e
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-07-08 15:47:17 +0530

    Time: 17 ms (26.07%) | Memory: 16.2 MB (21.29%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,32 +9,15 @@
  */
 class Solution {
 public:
-    TreeNode* find(TreeNode* root,TreeNode* p)
-    {
-        if(root == nullptr)
-            return nullptr;
-        if(root == p)
-            return root;
-        TreeNode* a;TreeNode* b;
-        a=find(root->left,p);
-        b=find(root->right,p);
-        if(a==nullptr && b == nullptr)
-            return nullptr;
-        else{
-            if(a !=nullptr)
-                return root->left;
-            else
-                return root->right;
-        }
-    }
-
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
        if(root == nullptr)
+            return nullptr;
+        if(root == p || root == q)
             return root;
-        TreeNode* a = find(root,p);
-        TreeNode* b= find(root,q);
-        if(a==root || b==root)  return root;
-        if(a==b)   return lowestCommonAncestor(a,p,q);
-       return root;
+            TreeNode* a=    lowestCommonAncestor(root->left,p,q);
+            TreeNode* b=    lowestCommonAncestor(root->right,p,q);
+        if(a == nullptr) return b;
+        if(b == nullptr) return a;
+        return root;
     }
 };
