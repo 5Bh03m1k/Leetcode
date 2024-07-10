11d36dbf49577ad8c04869fd3ad675e0a2e97bcd
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-07-10 16:16:33 +0530

    Time: 61 ms (14.71%) | Memory: 51 MB (13.94%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     vector<vector<int>> arr;
     vector<int> s;
+    vector<int> k;
     int sum(vector<int> s)
     {
         int a=0;
         for(int i=0 ; i<s.size() ; i++)     a+=s[i];
         return a;
     }
-    void fn(vector<int> k,int t,int i=0)
+    void fn(int t,int i=0)
     {
         int a=sum(s);
         if(a > t)  return;
         if(a == t){ arr.push_back(s);   return;}
         if(i>=k.size()) return;
         s.push_back(k[i]);
-        fn(k,t,i);
+        fn(t,i);
         s.pop_back();
-        fn(k,t,i+1);
+        fn(t,i+1);
 
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        fn(candidates,target);
+        k=candidates;
+        fn(target);
         return arr;
     }
 };
