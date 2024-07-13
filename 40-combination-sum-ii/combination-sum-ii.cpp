26646f6bf2cd3a136f7ed99936d4c27b2f685b62
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-07-14 05:27:19 +0530

    Time: 65 ms (5.03%) | Memory: 25.9 MB (6.00%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    
-    void fn(vector<pair<int,int>> table,int t , int sum = 0 , int itr = 0 , vector<int> a={})
+    vector<vector<int>> res; 
+    vector<pair<int,int>> table;
+    int t;
+    void fn( int sum = 0 , int itr = 0 , vector<int> a={})
     {
         if(sum == t)    {res.push_back(a);return;}
         if(sum > t)     return;
@@ -12,20 +13,20 @@ public:
             
         for(int i=0 ; i<=table[itr].second ; i++)
         {
-            fn(table , t , sum , itr+1 , a);
+            fn( sum , itr+1 , a);
             sum-=table[itr].first;
             a.pop_back();
         }
         return;
     }
-    vector<vector<int>> combinationSum2(vector<int>& can, int t) {
+    vector<vector<int>> combinationSum2(vector<int>& can, int tar) {
+        t = tar;
         unordered_map < int , int > hTable;
-        vector<pair<int,int>> table;
         for(int i=0 ; i<can.size() ; i++)
             hTable[can[i]]++;
         for(auto it=hTable.begin() ; it != hTable.end() ; it++)
             table.push_back({it->first,it->second});
-        fn(table,t);
+        fn();
         return res;
     }
 };
