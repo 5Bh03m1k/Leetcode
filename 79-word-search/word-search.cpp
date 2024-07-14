62094bce4cc11f7af10fc4d99be346d5fc96d0e0
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-07-15 04:01:14 +0530

    Time: 214 ms (83.12%) | Memory: 10.1 MB (71.28%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     vector<vector<char>> b;
     string w;
-    bool check(int i, int j, int itr, long long unsigned int &table) {
+    bool check(int i, int j, int itr, uint64_t table=0) {
         if (itr == w.size()) return true;
         if (i >= b.size() || i < 0) return false;
         if (j >= b[0].size() || j < 0) return false;
         int p=b[0].size()*i + j;
-        long long unsigned int o=1,love;
+        ///////hashmap using uint64_t/////////
+        uint64_t o=1,love;
         o=o<<p;
         love = table & o;
-         if (love != 0) return false;
+        if (love != 0) return false;
         if (b[i][j] != w[itr]) return false;
-         love = table;
+        love = table;
         table = table | o;
         bool res= check(i + 1, j, itr + 1,table) || check(i - 1, j, itr + 1,table) || check(i, j + 1, itr + 1,table) || check(i, j - 1, itr + 1,table);
         table = love;
@@ -20,14 +21,16 @@ public:
     }
 
     bool exist(vector<vector<char>>& board, string word) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         b = board;
         w = word;
-        long long unsigned int t=0;
         int a=board.size(),b=board[0].size(),c=a*b;
         for (int i = 0; i < a; i++) {
             for (int j = 0; j < b; j++) {
                 if (word[0] == board[i][j]) {
-                    if (check(i, j, 0,t))
+                    if (check(i, j, 0))
                         return true;
                 }
             }
