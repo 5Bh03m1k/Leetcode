625d7490ab2bf9bec99e2361c57a659ceb818912
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-07-18 19:56:27 +0530

    Time: 2 ms (95.76%) | Memory: 44.7 MB (55.42%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
@@ -1,35 +1,35 @@
 class Solution {
+    private int count = 0;
+    private final int MAX_DISTANCE = 10;
+
     public int countPairs(TreeNode root, int distance) {
-        Map<TreeNode, List<TreeNode>> map = new HashMap<>();
-        List<TreeNode> leaves = new ArrayList<>();
-        findLeaves(root, new ArrayList<>(), leaves, map);
-        int res = 0;
-        for (int i = 0; i < leaves.size(); i++) {
-            for (int j = i + 1; j < leaves.size(); j++) {
-                List<TreeNode> list_i = map.get(leaves.get(i));
-                List<TreeNode> list_j = map.get(leaves.get(j));
-                for (int k = 0; k < Math.min(list_i.size(), list_j.size()); k++) {
-                    if (list_i.get(k) != list_j.get(k)) {
-                        int dist = list_i.size() - k + list_j.size() - k;
-                        if (dist <= distance) res++;
-                        break;
-                    }
-                }
-            }
-        }
-        return res;
+        dfs(root, distance);
+        return count;
     }
 
-    private void findLeaves(TreeNode node, List<TreeNode> trail, List<TreeNode> leaves, Map<TreeNode, List<TreeNode>> map) {
-        if (node == null) return;
-        List<TreeNode> tmp = new ArrayList<>(trail);
-        tmp.add(node);
+    private int[] dfs(TreeNode node, int distance) {
+        if (node == null) return new int[MAX_DISTANCE + 1];
+        
         if (node.left == null && node.right == null) {
-            map.put(node, tmp);
-            leaves.add(node);
-            return;
+            int[] res = new int[MAX_DISTANCE + 1];
+            res[1] = 1;
+            return res;
         }
-        findLeaves(node.left, tmp, leaves, map);
-        findLeaves(node.right, tmp, leaves, map);
+
+        int[] left = dfs(node.left, distance);
+        int[] right = dfs(node.right, distance);
+
+        for (int i = 1; i <= distance; i++) {
+            for (int j = 1; j <= distance - i; j++) {
+                count += left[i] * right[j];
+            }
+        }
+
+        int[] res = new int[MAX_DISTANCE + 1];
+        for (int i = 1; i < MAX_DISTANCE; i++) {
+            res[i + 1] = left[i] + right[i];
+        }
+
+        return res;
     }
 }
