15749651daff435eddb845f22b64e3aee239e539
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-07-25 17:19:37 +0530

    Time: 81 ms (92.39%) | Memory: 69.5 MB (83.48%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
+    int n[100001]={0};
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
+        int max = INT_MIN , min = INT_MAX;
+        for(int i=0 ; i<nums.size() ; i++)
+        {
+            int indx = nums[i]+50000;
+           max = indx>max?indx:max;
+           min = indx < min ? indx : min;
+           n[indx]++;
+        }
+        nums.clear();
+        for(int i = min ; i<=max ; i++)
+        {
+            while(n[i]--)
+            nums.push_back(i-50000);
+        }
         return nums;
     }
 };
