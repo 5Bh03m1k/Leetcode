735da8ee8d33f31be39d02bac62b90e2cbd5f301
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-09-22 14:01:34 +0530

    Time: 16 ms (54.75%) | Memory: 14.1 MB (48.33%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int num;
+    void fn(int x , vector<int>&a)
+    {
+        if(num < x) return;
+        a.push_back(x);
+        x*=10;
+        for(int i=0 ; i<10 ; i++)
+            fn(x+i,a);
+
+    }
+    vector<int> lexicalOrder(int n) {
+        num = n;
+        vector<int> sol;
+        for(int i=1 ; i < 10 ; i++)
+            fn(i,sol);
+        return sol;
+        }
+};
