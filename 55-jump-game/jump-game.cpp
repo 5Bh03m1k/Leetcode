f9d959aa49d22a1ff370f64493a55db5ef56d76e
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-10-16 00:04:22 +0530

    Time: 62 ms (19.28%) | Memory: 54.4 MB (6.24%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        // if(nums.size() == 1)    return true;
-        for(int i=0 ; i<nums.size();i++)
-            nums[i]+=i;
-        int n;
         priority_queue<int> pq;
         pq.push(nums[0]);
+        int i=0;
         while(true)
         {
-            int i=0;
             for( i ; i<=pq.top() ; i++)
-               { if( pq.top() >= nums.size()-1) return true;pq.push(nums[i]);}
-            cout<<pq.top()<<i;
+               { if( pq.top() >= nums.size()-1) return true;pq.push(nums[i]+i);}
             if(i >= pq.top())   return false;
-            // if( pq.top() >= nums.size()-1) return true; 
         }
     }
 };
