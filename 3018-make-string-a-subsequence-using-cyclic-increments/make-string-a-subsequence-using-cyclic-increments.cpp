67fafe2387af151e6d6ca11c73d97e79a849a01c
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2024-12-04 23:03:17 +0530

    Time: 4 ms (51.93%) | Memory: 17.2 MB (7.53%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool canMakeSubsequence(string str1, string str2) {
-        queue<char> q;
+        int j=0;
         char ch;
-
-        for(int i=0 ; i<str2.size() ; i++)
-            q.push(str2[i]);
     
         for(int i=0 ; i<str1.size() ; i++)
         {
-            if(q.size() > 0)
+            if(str2.size() > j)
             {
-                if(str1[i] == q.front() || (char)((int)str1[i] + 1) == q.front() || (str1[i] == 'z' && q.front() == 'a'))
-                    q.pop();
+                if( str1[i] == str2[j] || (char)((int)str1[i] + 1) == str2[j] || (str1[i] == 'z' && str2[j] == 'a') )
+                    j++;
             }
         }
 
-        if(q.size() == 0)   return true;
+        if(str2.size() == j)   return true;
 
         return false;
     }
