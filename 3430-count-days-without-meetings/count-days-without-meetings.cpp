1a7f593102d0b780919c59773bb2f482b0e56411
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-03-25 02:42:32 +0530

    Time: 71 ms (53.29%) | Memory: 126.4 MB (80.45%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int countDays(int d, vector<vector<int>>& m) {
         sort(m.begin(),m.end());
-        stack<int> st;
-        st.push(m[0][0]);
-        st.push(m[0][1]);
+        vector<int> st;
+        st.push_back(m[0][0]);
+        st.push_back(m[0][1]);
         for(int i=1;i<m.size();i++)
         {
-            if(m[i][0]<=st.top())
+            if(m[i][0]<=st.back())
             {
-                if(m[i][1]>st.top())
+                if(m[i][1]>st.back())
                 {
-                    st.pop();
-                    st.push(m[i][1]);
+                    st.pop_back();
+                    st.push_back(m[i][1]);
                 }
             }
             else
             {
-                st.push(m[i][0]);
-                st.push(m[i][1]);
+                st.push_back(m[i][0]);
+                st.push_back(m[i][1]);
             }
 
         }
@@ -26,10 +26,10 @@ public:
         int t=0;
         while(st.size()>0)
         {
-            t+=st.top()+1;
-            st.pop();
-            t-=st.top();
-            st.pop();
+            t+=st.back()+1;
+            st.pop_back();
+            t-=st.back();
+            st.pop_back();
         }
         return d-t;
         
