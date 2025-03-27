4cf3025daef2abc33249e541467f9570f00d5afd
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-03-28 05:21:51 +0530

    Time: 83 ms (98.40%) | Memory: 8.7 MB (99.22%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,26 +1,25 @@
+vector<bool> arr(5000000, true);
+void prime(vector<bool>& arr) {
+    arr[0] = arr[1] = false;
+    arr[2] = true;
+    for (int i = 2; i * i < 4999999; i++) {
+        if (arr[i]) {
+            for (int j = 2 * i; j <= 4999999; j += i) {
+                if (arr[j])
+                    arr[j] = false;
+            }
+        }
+    }
+}
 class Solution {
 public:
     int countPrimes(int n) {
-        if(n <= 2)   return 0;
-        vector<bool> arr(n , true);       
-        n--;
-        arr[0]=arr[1]=false;
-        arr[2] =true;
-        for(int i=2 ; i*i <= n ; i++)
-        {
-            if(arr[i]){
-                for(int j=2*i ; j<= n ; j+=i)
-                {
-                    if(arr[j])arr[j]=false;
-                }
-            }
-            
-        }
-        int count=0;
-        for(auto i:arr){
-            if(i) count++;
+        if(arr[0])  prime(arr);
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i])
+                count++;
         }
         return count;
-
     }
 };
