2d14bd9a0c0204c86499e0430359684a9f7befa7
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-03-27 19:44:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (57.09%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        unordered_set<int> hash;
+        /*unordered_set<int> hash;
         hash.insert(6);
         hash.insert(28);
         hash.insert(496);
         hash.insert(8128);
-        hash.insert(33550336);
+        hash.insert(33550336);*/
 
-        if(hash.find(num) != hash.end())    return true;
-        return false;
+        switch(num)
+        {
+            case 6:return true;
+            case 28:return true;
+            case 496:return true;
+            case 8128:return true;
+            case 33550336:return true;
+            default :return false;
+        }
     }
 };
