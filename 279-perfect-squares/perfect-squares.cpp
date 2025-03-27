75dee68240899bff450efb16b5504005a706cd7b
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-03-28 01:51:29 +0530

    Time: 13 ms (95.29%) | Memory: 9.9 MB (89.65%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,15 @@
+unordered_map<int,int> table;
+class Solution {
+
+public:
+    int numSquares(int n ) {
+        if(n == 0)  return 0;
+        if(table.count(n) != 0) return table[n];
+        int m = INT_MAX,s=sqrt(n);
+        if(s*s == n)    return 1;
+        for(int i=1 ; i*i<n ; i++)
+            m = min(m,numSquares(n-(i*i))+1);
+        table[n] = m;
+        return m;
+    }
+};
