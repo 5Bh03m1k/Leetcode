c6c154ec5859b24e4601b1b1af3878a2d4c1213c
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-03-28 02:18:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (95.39%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,15 +1,28 @@
-unordered_map<int,int> table;
 class Solution {
-
 public:
-    int numSquares(int n ) {
-        if(n == 0)  return 0;
-        if(table.count(n) != 0) return table[n];
-        int m = INT_MAX,s=sqrt(n);
-        if(s*s == n)    return 1;
-        for(int i=s ; i*i>=s ; i--)
-            m = min(m,numSquares(n-(i*i))+1);
-        table[n] = m;
-        return m;
+    bool is_perfect(int n)
+    {
+        int s = sqrt(n);
+
+        if(s*s == n)    return true;
+        return false;
+    }
+
+    int numSquares(int n) {
+        //////////langrage 4 square theorem///////////
+        /////////is perfect square == 1////////
+        if(is_perfect(n))   return 1;
+        /////////n=a^2+b^2///////////
+        for(int i=1 ; i<sqrt(n);i++)
+        {
+            if(is_perfect(n - i*i)) return 2;
+        }
+        /////////////legends 4 square theorem ie. n = a^2 + b^2 + c^2 +d^2
+        while(n%4 == 0) n/=4;
+        if(n%8 == 7)    return 4;
+
+    //////////////////////////else return 3///////////////////
+        return 3;
+
     }
 };
