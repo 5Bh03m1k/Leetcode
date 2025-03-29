ce3f19ea127671a9eab6f9c688c3057ee00aa4ee
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-03-29 06:21:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (83.90%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0)    return false;
-        while(n != 1)
+        while(n > 0)
         {
-            if(n & 1 == 1)  return false;
+            if(n & 1)  
+            {
+                if(n == 1)  return true;
+                return false;
+            }
             n >>= 1;
         }
-        return true;
+        return false;
     }
 };
