ad436af74d7fb866291ffbdf95ab168ca5d9e6a4
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-04-02 04:16:20 +0530

    Time: 2 ms (82.26%) | Memory: 119.5 MB (95.59%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -2,8 +2,8 @@ class Solution {
 
 public:
     long long mostPoints(vector<vector<int>>& a) {
-        vector<long long> map(a.size());
-       long long maxi = 0;
+        long long* map = (long long*)malloc(a.size() * sizeof(long long));
+        long long maxi = 0;
         for(int i=a.size()-1 ; i>=0 ; i--){
             map[i] = max((a[i][0] + ((i+a[i][1]+1) < a.size()?map[i+a[i][1]+1] : 0)),maxi);
             maxi = map[i];
