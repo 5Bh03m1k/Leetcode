8a7e965d72358e94bcf236f5c8c4506130393093
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-04-17 09:18:33 +0530

    Time: 1 ms (42.39%) | Memory: 16.1 MB (7.30%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
-       /* unordered_map<int , vector<int>> table;
+       unordered_map<int , vector<int>> table;
 
         for(int i=0 ; i<nums.size() ; i++)
             table[nums[i]].push_back(i);
@@ -10,26 +10,20 @@ public:
 
         for(auto it = table.begin() ; it != table.end() ; it++){
 
-            int s = it->second.size();
+            //int s = it->second.size();
 
             for(int i=0 ; i < it->second.size()  ; i++){
-                 if(it->second[i] % k == 0){
-                    count += s-1;
-                    s--;
+                for(int j=i+1 ; j < it->second.size() ; j++){
+                 if((it->second[i]*it->second[j]) % k == 0){
+                    count ++;
+                    //s--;
                  }
+                }
             }
         }
 
-        return count;*/
-        int sum = 0;
-        for(int i=0 ; i<nums.size() ; i++)
-        {
-            for(int j=i+1 ; j<nums.size() ; j++)
-            if(nums[i] == nums[j] && (i*j)%k == 0)
-                sum++;
-        }
-        return sum;
-
+        return count;
+        
         
     }
 };
