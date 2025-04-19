5baa4a7f7c3b816a8bf1f5a4d2fcaceed0775a8f
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-04-20 00:34:54 +0530

    Time: 53 ms (64.00%) | Memory: 60.4 MB (73.14%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -3,16 +3,13 @@ public:
     
     long long countFairPairs(vector<int>& arr, int lower, int upper) {
         sort(arr.begin(),arr.end());
-        long long sum = 0;
-        for(auto i : arr)   cout<<i<<" ";
-        cout<<endl;
+        long long sum = 0 , it , it1;
         for(auto i=arr.begin() ; i!= arr.end(); i++)
         {
-            auto it = lower_bound(i+1 , arr.end() , lower - *i);
-            auto it1 = upper_bound(i+1 , arr.end() , upper - *i);
-            int n = arr.end() - it , b = arr.end() - it1 ;
-            cout<<n <<" "<<b<<endl;
-            sum += n - b;
+            it = lower_bound(i+1 , arr.end() , lower - *i) - arr.begin();
+            it1 = upper_bound(i+1 , arr.end() , upper - *i) - arr.begin();
+            
+            sum += it1 - it;
         }
         return sum;
     }
