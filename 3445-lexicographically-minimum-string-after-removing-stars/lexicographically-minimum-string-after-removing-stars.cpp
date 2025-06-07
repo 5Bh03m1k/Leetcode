d76e5d598b8736dbe7eb51457ea73327f00e36b5
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-06-07 23:13:58 +0530

    Time: 267 ms (23.05%) | Memory: 109.7 MB (15.91%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
     string clearStars(string s) {
-        priority_queue<char , vector<char> , greater<char> > pq;
-        unordered_map<char , vector<int>> table;
-        priority_queue<int , vector<int> , greater<int>> st;
+        map<char, vector<int>> charMap; // sorted by default
+        vector<bool> deleted(s.size(), false);
 
-        for(int i = 0 ; i < s.size() ; i++)
-        {
-            if(s[i] != '*')
-            {
-                pq.push(s[i]);
-                table[s[i]].push_back(i);
-            }
-            
-            else{
-                st.push(table[pq.top()].back());
-                table[pq.top()].pop_back();
-                pq.pop();
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] != '*') {
+                charMap[s[i]].push_back(i);
+            } else {
+                // Remove index of smallest character
+                auto it = charMap.begin();
+                int idx = it->second.back();
+                deleted[idx] = true;
+                it->second.pop_back();
+                if (it->second.empty()) {
+                    charMap.erase(it);
+                }
             }
         }
-        
-        string ans = "";
 
-        for(int i=0 ; i < s.size() ; i++)
-        {
-            if(st.size() > 0 && st.top() == i)
-            {
-                st.pop();
-                goto tag;
+        string result;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] != '*' && !deleted[i]) {
+                result += s[i];
             }
-
-            if(s[i] == '*')
-                goto tag;
-            
-             ans.push_back(s[i]);
-
-            tag:
         }
 
-        return ans;
-
+        return result;
     }
 };
