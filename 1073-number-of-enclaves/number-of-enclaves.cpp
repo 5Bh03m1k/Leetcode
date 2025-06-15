7a0aff14e9ed037be0a75f9957dfd36bd1435088
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-06-16 00:37:08 +0530

    Time: 1 ms (97.42%) | Memory: 36.8 MB (42.26%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,32 +1,31 @@
 class Solution {
+private:
+    void dfs(vector<vector<int>>&g , int x , int y)
+    {
+        if(x < 0 || x >= g.size() || y < 0 || y >= g[0].size() || g[x][y] == 0) return;
+
+        g[x][y] = 0;
+
+        dfs(g,x-1,y);
+        dfs(g,x+1,y);
+        dfs(g,x,y-1);
+        dfs(g,x,y+1);
+    }
+
 public:
     int numEnclaves(vector<vector<int>>& g) {
-        queue<pair<int,int>> q;
-        
         for(int i = 0 ; i < g.size() ; i++)
         {
-            if( g[i][0] == 1 ) g[i][0] = 0 , q.push({i,0});
-            if(g[i][g[0].size()-1] == 1)  g[i][g[0].size()-1] = 0,  q.push({i,g[0].size()-1});
+            if( g[i][0] == 1 ) dfs(g,i,0);
+            if(g[i][g[0].size()-1] == 1) dfs(g,i,g[0].size()-1);
         } 
 
         for(int i = 0 ; i < g[0].size() ; i++)
         {
-            if( g[0][i] == 1 ) g[0][i] = 0, q.push({0,i});
-            if(g[g.size()-1][i] == 1) g[g.size()-1][i] = 0 ,   q.push({g.size()-1,i});
+            if( g[0][i] == 1 ) dfs(g,0,i);
+            if(g[g.size()-1][i] == 1) dfs(g,g.size()-1,i);
         } 
 
-        while(!q.empty())
-        {
-            int x = q.front().first , y = q.front().second;
-
-           if(x-1 >= 0           && g[x-1][y] == 1) g[x-1][y] = 0 , q.push({x-1,y}) ;
-           if(x+1 < g.size()     && g[x+1][y] == 1) g[x+1][y] = 0 , q.push({x+1,y}) ;
-           if(y-1 >= 0           && g[x][y-1] == 1) g[x][y-1] = 0 , q.push({x,y-1}) ;
-           if(y+1 < g[0].size()  && g[x][y+1] == 1) g[x][y+1] = 0 , q.push({x,y+1}) ;
-
-           q.pop();
-        }
-        
         int c = 0;
 
         for(auto i:g)
