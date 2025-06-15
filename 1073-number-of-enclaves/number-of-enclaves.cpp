d9b5bda7081c43c76828ad00fba83d74e266a3ed
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-06-16 00:16:23 +0530

    Time: 7 ms (77.06%) | Memory: 31.5 MB (87.25%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int numEnclaves(vector<vector<int>>& g) {
-        int cnt = 0,count = 0;
         queue<pair<int,int>> q;
-
-        for(auto i:g)
-        {
-            for(auto j:i)
-                cnt += j;
-        }
-
+        
         for(int i = 0 ; i < g.size() ; i++)
         {
             if( g[i][0] == 1 ) g[i][0] = 0 , q.push({i,0});
@@ -22,20 +15,25 @@ public:
             if(g[g.size()-1][i] == 1) g[g.size()-1][i] = 0 ,   q.push({g.size()-1,i});
         } 
 
-        count += q.size();
-
         while(!q.empty())
         {
             int x = q.front().first , y = q.front().second;
 
-           if(x-1 >= 0           && g[x-1][y] == 1) g[x-1][y] = 0 , q.push({x-1,y}) , count++;
-           if(x+1 < g.size()     && g[x+1][y] == 1) g[x+1][y] = 0 , q.push({x+1,y}) , count++;
-           if(y-1 >= 0           && g[x][y-1] == 1) g[x][y-1] = 0 , q.push({x,y-1}) , count++;
-           if(y+1 < g[0].size()  && g[x][y+1] == 1) g[x][y+1] = 0 , q.push({x,y+1}) , count++;
+           if(x-1 >= 0           && g[x-1][y] == 1) g[x-1][y] = 0 , q.push({x-1,y}) ;
+           if(x+1 < g.size()     && g[x+1][y] == 1) g[x+1][y] = 0 , q.push({x+1,y}) ;
+           if(y-1 >= 0           && g[x][y-1] == 1) g[x][y-1] = 0 , q.push({x,y-1}) ;
+           if(y+1 < g[0].size()  && g[x][y+1] == 1) g[x][y+1] = 0 , q.push({x,y+1}) ;
 
            q.pop();
         }
-        cout<<cnt;
-        return cnt - count;
+        
+        int c = 0;
+
+        for(auto i:g)
+        {
+            for(auto j:i)
+                 c += j;
+        }
+        return c ;
     }
 };
