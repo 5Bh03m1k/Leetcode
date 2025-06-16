6fef968f8725b0d1b8c7f95a16813ec7689d2f66
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-06-16 21:14:15 +0530

    Time: 3 ms (68.77%) | Memory: 12.3 MB (30.09%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -21,67 +21,28 @@ public:
 
 class Solution {
 private:
-    unordered_set<Node*> table;
-
-    void makelist( Node* node)
+    unordered_map<Node*,Node*> table;
+    Node* dfs(Node* node)
     {
-        if(node == nullptr)
-            return;
-        
-        table.insert(node);
-        
-        for(auto i:node->neighbors)
-        {
-            if(table.find(i) == table.end())
-                makelist(i);
-        }
+        if(node == nullptr) return nullptr;
 
-    }
-
-    unordered_map<Node*,Node*> r_table;
-
-    void makeNode()
-    {
-        
-        for(auto it = table.begin() ; it != table.end() ; it++)
-        {
-            int r = (*it)->val;
-            Node* n = new Node(r);
-            r_table[*it] = n;
-        }
-        table.clear();
-    }
-
-    void connect_node(Node* node)
-    {
-        vector<Node*>al;
-        if(node == nullptr)
-        {
-            //r_table[node]->neighbors = al;
-            return;
-        }
-        table.insert(node);
-        for(auto i:node->neighbors)
-        {
-            al.push_back(r_table[i]);
-        }
-
-        r_table[node]->neighbors = (al);
+        Node* n = new Node;
+        n->val = node->val;
+        table[node] = n;
 
         for(auto i:node->neighbors)
         {
             if(table.find(i) == table.end())
-                connect_node(i);
+                n->neighbors.push_back(dfs(i));
+            else
+                n->neighbors.push_back(table[i]);
         }
-    }
 
+        return n;
+    }
 public:
     Node* cloneGraph(Node* node) {
-        vector<vector<int>> v;
-        makelist(node);
-        makeNode();
-        connect_node(node);
-        return r_table[node];
+        return dfs(node);
 
     }
 };
