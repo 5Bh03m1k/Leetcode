0aebb5d513f5c9974c759c852eb81841d5a93233
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-06-16 16:15:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (71.76%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
-        int t = -1;
-        priority_queue<int> pq;
-        pq.push(nums[nums.size()-1]);
+        int t = -1 , m = nums[0]; 
 
-        for(int i = nums.size()-2 ; i > -1 ; i--)
+        for(auto i:nums)
         {
-            if(pq.top() > nums[i])
-                t = t > pq.top() - nums[i]? t : pq.top() - nums[i];
-
-            pq.push(nums[i]);
+            t = ( m < i ) ? ( ( t < i - m) ? i - m : t ) : t;
+            m = m > i ? i : m;
         }
 
         return t;
