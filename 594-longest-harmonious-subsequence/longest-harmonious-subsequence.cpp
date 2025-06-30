d732998a3c591d99df4128357758f7e79c24026b
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-06-30 06:08:11 +0530

    Time: 42 ms (24.49%) | Memory: 43.9 MB (47.36%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -9,22 +9,9 @@ public:
 
         for(auto it = table.begin() ; it != table.end() ; it++)
         {
-            if(!table.count(it->first + 1) )
-            {
-                if(!table.count(it->first - 1) )
-                    continue;
-                
-                else
-                    tmp = table[it-> first -1];
-            }
 
-            else{
-                if(!table.count(it->first - 1) )
-                    tmp = table[it-> first + 1];
-
-                else
-                    tmp = max(table[it-> first + 1],table[it-> first - 1]);
-            }
+            tmp = (!table.count(it->first + 1) && !table.count(it -> first - 1))? -it->second : (!table.count(it->first+1) ? table[it->first -1] : (!table.count(it->first-1)?table[it->first+1]:max(table[it-> first + 1],table[it-> first - 1])));
+            
             
             r = max(tmp + it->second , r);
         }
