ed0617ae875ef67af2577d66a5d429e8c1b89708
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-07-07 13:11:42 +0530

    Time: 598 ms (5.07%) | Memory: 603.5 MB (5.07%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,6 +1,32 @@
+class h_table {
+private:
+    std::bitset<50000> a; // Use appropriate size
+    int s = 0;
+
+public:
+    void insert(int n) {
+        a.set(n);
+        s++;
+    }
+
+    void erase(int n) {
+        a.reset(n);
+        s--;
+    }
+
+    bool find(int n) {
+        return a.test(n);
+    }
+
+    int size()
+    {
+        return s;
+    }
+};
+
 class Solution {
 private:
-int dfs(int n , vector<vector<int>> &a ,vector<bool>&vis ,vector<unordered_set<int>>&p)
+int dfs(int n , vector<vector<int>> &a ,vector<bool>&vis ,vector<h_table>&p)
 {
     int w = 0;
     vis[n] = true;
@@ -8,7 +34,7 @@ int dfs(int n , vector<vector<int>> &a ,vector<bool>&vis ,vector<unordered_set<i
     {
         if(vis[a[n][i]] == false)
         {
-            if(p[n].size() && p[n].find(a[n][i]) != p[n].end())
+            if(p[n].size() && p[n].find(a[n][i]))
                 w++;
             w += dfs(a[n][i],a,vis,p);
         }
@@ -19,7 +45,7 @@ int dfs(int n , vector<vector<int>> &a ,vector<bool>&vis ,vector<unordered_set<i
 public:
     int minReorder(int n, vector<vector<int>>& ed) {
         vector<vector<int>> al(n);
-        vector<unordered_set<int>> p(n);
+        vector<h_table> p(n);
 
         vector<bool>vis(n,false);
 
