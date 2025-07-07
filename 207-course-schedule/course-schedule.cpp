dfdd6f88d932b1a1c1269690214cda4f65c10c0f
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-07-07 12:25:44 +0530

    Time: 3 ms (79.62%) | Memory: 20.1 MB (41.14%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,25 +1,27 @@
 class Solution {
 private:
-    bool dfs(vector<vector<int>>&a , int n,unordered_set<int>&vis , vector<bool>&ic)
+    bool dfs(vector<vector<int>>&a , int n,bitset<2000>&vis , bitset<2000>&ic)
     {
-        if(vis.find(n) != vis.end()) return false;
-        vis.insert(n);
+        if(vis.test(n)) return false;
+        vis.set(n);
 
 
         for(auto i : a[n])
         {
-            if(ic[i] == true && dfs(a,i,vis,ic) == false)   return false;
+            if(ic.test(i) && dfs(a,i,vis,ic) == false)   return false;
         }
-        ic[n] = false;
-        vis.erase(n);
+        ic.reset(n);
+        vis.reset(n);
         return true;
     }
 public:
     bool canFinish(int s, vector<vector<int>>& p) {
         
         vector<vector<int>> al(s);
-        unordered_set<int> vis;
-        vector<bool> ic(s,true); ////////////////memorization matrix////////////
+        bitset<2000>vis;
+        bitset<2000> ic;
+        ic.set();
+    
         for(auto i:p)
             al[i[1]].push_back(i[0]);
 
@@ -27,7 +29,7 @@ public:
 
         for(int i=0 ;i < s ;i++)
         {
-            if(ic[i] && dfs(al,i,vis,ic) == false)  return false;
+            if(ic.test(i) && dfs(al,i,vis,ic) == false)  return false;
          
         }
 
