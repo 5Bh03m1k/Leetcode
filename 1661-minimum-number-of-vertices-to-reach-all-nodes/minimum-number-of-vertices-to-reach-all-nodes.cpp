ffdc3daf7eec6a17f3f5bd344ff5d6ba8297cfb7
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-07-07 11:41:25 +0530

    Time: 5 ms (61.58%) | Memory: 98.8 MB (88.10%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<bool> a(n,true);
+        bool* a = (bool*)calloc(n,sizeof(int));
         vector<int> ans;
 
         for(int i = 0 ; i < edges.size() ; i++)
-            a[edges[i][1]] = false;
+            a[edges[i][1]] = 1;
         
         for(int i=0 ; i<n ; i++)
         {
-            if(a[i])
+            if(a[i] == 0)
                 ans.push_back(i);
         }
         return ans;
