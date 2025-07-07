18210a73b59bcf1778444a46a680bdc5220005da
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-07-07 12:28:15 +0530

    Time: 25 ms (52.56%) | Memory: 35.9 MB (66.84%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -1,9 +1,30 @@
+#include <bitset>
+
+class h_table {
+private:
+    std::bitset<100> a; // Use appropriate size
+
+public:
+    void insert(int n) {
+        a.set(n);
+    }
+
+    void erase(int n) {
+        a.reset(n);
+    }
+
+    bool find(int n) {
+        return a.test(n);
+    }
+};
+
+
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         vector<int> r(n,0);
         vector<int>sr(n);
-        vector<unordered_set<int>> al(n);
+        vector<h_table> al(n);
 
         for(auto i:roads)
         {
@@ -14,14 +35,17 @@ public:
         }
 
         int m = -1;
+        
         for(int i = 0 ; i < n ; i++)
         {
             for(int j = i+1 ; j < n ; j++)
             {
-                m = max(m,al[i].find(j) != al[i].end() ? r[i] + r[j] -1 : r[i] +r[j]);
+                m = max(m,al[i].find(j)? r[i] + r[j] -1 : r[i] +r[j]);
             }
         }
 
         return m;
+
+        //return m;
     }
 };
