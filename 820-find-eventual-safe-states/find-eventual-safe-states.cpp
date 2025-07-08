70394522a7bd6526783bc5047d6885b14eb76054
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-07-08 12:38:25 +0530

    Time: 46 ms (56.72%) | Memory: 66.3 MB (21.38%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& al) {
+        int n = al.size();  
+
+        vector<int> out(n);
+        vector<vector<int>> al_new(n);
+
+        for(int i = 0 ; i < al.size() ; i++)
+        {
+            out[i] = al[i].size();
+
+            for(auto j:al[i])
+            {
+                al_new[j].push_back(i);
+            }
+        }   
+
+        queue<int> q;
+        vector<int> topo;
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(out[i] == 0) q.push(i);
+        }
+
+        while(!q.empty())
+        {
+            int k = q.front();
+            topo.push_back(k);
+
+            for(auto i:al_new[k])
+            {
+                out[i]--;
+                if(out[i] == 0) q.push(i);
+            }
+
+            q.pop();
+        }
+
+        sort(topo.begin(),topo.end());
+        
+        return topo;
+
+
+    }
+};
