b7c85425836ae7fc5f00df2ac5cad713190d034d
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-07-08 06:57:21 +0530

    Time: 4 ms (24.37%) | Memory: 13.7 MB (29.27%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,8 +1,8 @@
 class Solution {
 private:
-    bool cycle(vector<vector<int>>&al , int n , int prev , unordered_set<int>&u , deque<int>&dq)
+    bool cycle(vector<vector<int>>&al , int n , int prev , bitset<1000>&u , deque<int>&dq)
     {
-        if(u.find(n) != u.end()){
+        if(u.test(n)){
             while(dq.size() > 0)
             {
                 if(dq.front() == n) break;
@@ -12,15 +12,15 @@ private:
                 return false;
         }
 
-        u.insert(n);
+        u.set(n);
         dq.push_back(n);
 
-        for(auto i:al[n])
+        for(int i = 0 ; i < al[n].size() ; i++)
         {
-            if(i != prev && cycle(al,i,n,u,dq) == false) return false;
+            if(al[n][i] != prev && cycle(al,al[n][i],n,u,dq) == false) return false;
         }
         dq.pop_back();
-        u.erase(n);
+        u.reset(n);
 
         return true;
     }
@@ -39,7 +39,7 @@ public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int c = 0 , n = edges.size();
         vector<vector<int>> al(n);
-        unordered_set<int> u;
+        bitset<1000>u;
         deque<int> dq;
 
         list(al,edges);
@@ -48,17 +48,17 @@ public:
         //
 
         cycle(al,0,-1,u,dq);
-        u.clear();
+        u.reset();
         while(dq.size() > 0 )
         {
-            u.insert(dq.front());
+            u.set(dq.front());
             cout<<dq.front();
             dq.pop_front();
         }
 
         for(int i = edges.size() - 1 ; i>= 0 ; i--)
         {
-            if(u.find(edges[i][0]-1) != u.end() && u.find(edges[i][1]-1) != u.end())
+            if(u.test(edges[i][0]-1) && u.test(edges[i][1]-1))
                 return {edges[i][0] , edges[i][1]};
         }
 
