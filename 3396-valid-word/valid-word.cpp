87640d6cc96a06959a313fca3ad31442d32ab57a
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-07-15 13:25:47 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (67.48%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -4,27 +4,23 @@ public:
         if(word.size() < 3) return false;
         bool v = true , c = true ;
 
-        for(auto& i:word)
+        for(int i = 0 ; i < word.size() ; i++)
         {
-            if(isalnum(i) == false)    return false;
-            switch(i)
+            if(!isalnum(word[i]))    return false;
+            switch(word[i])
             {
-                case 'a': v = false;break;
-                case 'e': v = false;break;
-                case 'i':v = false;break;
-                case 'o':v = false;break;
-                case 'u':v = false;break;
-                case 'A': v = false;break;
-                case 'E': v = false;break;
-                case 'I':v = false;break;
-                case 'O':v = false;break;
-                case 'U':v = false;break;
-                default: if(isdigit(i) == false)    c = false;
+                case 'a' :v = false;break;case 'A': v = false;break;
+                case 'e' :v = false;break;case 'E': v = false;break;
+                case 'i' :v = false;break;case 'I':v = false;break;
+                case 'o' :v = false;break;case 'O':v = false;break;
+                case 'u' :v = false;break;case 'U':v = false;break;
+                default: if(!isdigit(word[i]))    c = false;
             }
 
         }
 
-        if(v || c)  return false;
-        return true;
+        cout<<v<<c;
+
+        return !(v || c);
     }
 };
