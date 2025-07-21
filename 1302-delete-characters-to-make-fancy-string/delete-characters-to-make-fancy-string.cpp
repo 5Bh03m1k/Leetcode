5fe55ab4c9aa689c2a172d770560521d4f6e9748
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-07-22 00:47:40 +0530

    Time: 43 ms (18.56%) | Memory: 44.7 MB (13.57%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        unordered_map<char ,int> table;
+        vector<int> table(26,0);
         int tmp = 0;
         string sh = "";
 
         if(s.size() < 3) return s;
 
         for(int i = 0 ; i < 3 ; i++)
-            table[s[i]]++;
+            table[s[i] -'a']++;
         
 
         for(int i = 3 ; i < s.size() ; i++)
         {
             tmp = i-3;
-            if(table[s[tmp]] < 3)
+            if(table[s[tmp]-'a'] < 3)
                 sh += s[tmp];
-            table[s[i]]++;
-            table[s[tmp]]--;
+            table[s[i]-'a']++;
+            table[s[tmp]-'a']--;
         }
         tmp = s.size() - 3;
         while(tmp < s.size())
         {
-            if(table[s[tmp]] < 3)
+            if(table[s[tmp]-'a'] < 3)
                 sh += s[tmp];
-            table[s[tmp]]--;
+            table[s[tmp]-'a']--;
             tmp ++;
         }
 
