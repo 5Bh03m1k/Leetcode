46e796a9d3220f7514224bd6b5d33d2f3b6dc13d
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-07-22 16:56:01 +0530

    Time: 4 ms (98.01%) | Memory: 93.1 MB (98.41%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int p1=0;
-        unordered_set<int> table;
+        bitset<10001> table;
         int sum = 0;
         int max = 0;
 
         for(int i = 0 ; i < nums.size() ; i++)
         {
-            while(table.find(nums[i]) != table.end())
+            while(table.test(nums[i]))
             {
-                table.erase(nums[p1]);
+                table.reset(nums[p1]);
                 sum -= nums[p1];
                 p1++;
             }
 
-            table.insert(nums[i]);
+            table.set(nums[i]);
             sum += nums[i];
             max = max > sum ? max :sum;
         }
