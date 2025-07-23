ff4c585506ec1c0b0178a9cc186477416bf11e28
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-07-24 03:22:00 +0530

    Time: 16 ms (89.97%) | Memory: 65.8 MB (76.54%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -0,0 +1,77 @@
+class p{
+        vector<vector<bool>> hash;
+    public:
+        p(int n)
+        {
+            vector<vector<bool>> Hmap(n,vector<bool>(n,false));
+            hash = Hmap;
+        }
+
+        void dfs(vector<vector<int>>&al,vector<bool>&vis , int n)
+        {
+            if(vis[n])  return;
+
+            vis[n] = true;
+
+            for(auto& i:al[n])
+            {
+                hash[n][i] = true;
+                dfs(al,vis,i);
+
+                for(int it = 0 ; it < hash[i].size() ; it++)
+                {
+                    if(hash[i][it])
+                        hash[n][it] = true;
+                }
+                
+            }
+
+            return;
+        }
+
+        void graph(int n , vector<vector<int>>&arr)
+        {
+            vector<vector<int>> al(n);
+            vector<int> in(n,0);
+            vector<bool> vis(n,false);
+
+            for(auto& i:arr)
+            {
+                al[i[0]].push_back(i[1]);
+                in[i[1]]++;
+            }
+
+            int c = 0;
+
+            for(auto& i:in)
+            {
+                if( i == 0 ) dfs(al,vis,c);
+                c++;
+            }
+
+
+        }
+
+        bool query(int a , int b)
+        {
+            return hash[a][b];
+        }
+
+    };
+class Solution {  
+public:
+    vector<bool> checkIfPrerequisite(int nu, vector<vector<int>>& ps, vector<vector<int>>& queries) {
+
+        p it(nu);
+        it.graph(nu,ps);
+        //it.show();
+
+        vector<bool> arr;
+
+        for(auto& i:queries)
+            arr.push_back(it.query(i[0],i[1]));
+        
+
+        return arr;
+    }
+};
