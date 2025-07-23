25d3edfb89f502547ec117e93ec8bdde30656fbb
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-07-24 00:02:42 +0530

    Time: 30 ms (77.30%) | Memory: 28.1 MB (56.96%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,31 +1,4 @@
 class Solution {
-private:
-    string chk(string s , int n ,int &ans)
-    {
-        vector<char> st;
-
-        for(int i = 0 ; i < s.size() ; i++)
-        {
-            if(s[i] == 'b' && st.size() > 0 && st.back() == 'a')
-            {
-                ans += n;
-                st.pop_back();
-
-                continue;
-            }
-
-            st.push_back(s[i]);
-        }
-
-        string sh = "";
-        while(!st.empty())
-        {
-            sh += st.back();
-            st.pop_back();
-        }
-
-        return sh;
-    }
 public:
     int maximumGain(string s, int x, int y) {
         if(x < y)   
@@ -34,14 +7,36 @@ public:
             swap(x,y);
         }
 
+        int ans = 0;
 
-        int a = 0;
+        vector<char> st ,sn;
 
-        string s_n;
-         
-        s_n = chk(s,x,a);
-        s_n = chk(s_n,y,a);
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            if(s[i] == 'b' && st.size() > 0 && st.back() == 'a')
+            {
+                ans += x;
+                st.pop_back();
 
-        return a;
+                continue;
+            }
+
+            st.push_back(s[i]);
+        }
+
+        while(!st.empty())
+        {
+             if(st.back() == 'b' && sn.size() > 0 && sn.back() == 'a')
+            {
+                ans += y;
+                sn.pop_back();
+
+                goto tag;
+            }
+
+            sn.push_back(st.back());
+            tag: st.pop_back();
+        }
+        return ans;
     }
 };
