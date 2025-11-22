bbe5f6d2da33cda935a95ff9ce8bdff634908cf1
Soubhagya Bhowmik <74861436+5Bh03m1k@users.noreply.github.com>
2025-11-22 12:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (79.62%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -3,8 +3,8 @@ public:
     int minimumOperations(vector<int>& nums) {
         int cnt = 0;
 
-        for(auto& i:nums)
-            cnt+= i%3 == 0? 0 : 1 ;
+        for(int i = 0 ; i < nums.size() ; i++)
+            cnt+= nums[i]%3 == 0? 0 : 1 ;
         
         return cnt;
     }
